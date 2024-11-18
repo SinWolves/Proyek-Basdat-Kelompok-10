ec5a772b8804fe147dc918ee86ba22efc21c71b1
rielyta <desi@students.usu.ac.id>
2024-11-19 04:16:06 +0700

    edit font,hotel,address

--- view_administrators/admin.js
+++ view_administrators/admin.js
@@ -1,22 +1,50 @@
-// Toggle the sidebar and overlay
-document.getElementById('menu-toggle').addEventListener('click', function () {
-    const sidebar = document.querySelector('.sidebar'); 
-    sidebar.classList.toggle('active'); 
-    overlay.classList.toggle('active'); 
+
+const menuToggle = document.getElementById('menu-toggle');
+const sidebar = document.querySelector('.sidebar');
+const overlay = document.querySelector('.overlay');
+const closeIcon = document.querySelector('.close-icon');
+
+
+menuToggle.addEventListener('click', function () {
+  sidebar.classList.toggle('active'); 
+  overlay.classList.toggle('active'); 
 });
 
-// Close the sidebar when clicking the close icon
-document.querySelector('.close-icon').addEventListener('click', function () {
-    const sidebar = document.querySelector('.sidebar'); 
-    const overlay = document.querySelector('.overlay'); 
-    sidebar.classList.remove('active'); 
-    overlay.classList.remove('active'); 
+
+closeIcon.addEventListener('click', function () {
+  sidebar.classList.remove('active');
+  overlay.classList.remove('active');
 });
 
-// Close the sidebar when clicking on the overlay
-document.querySelector('.overlay').addEventListener('click', function () {
-    const sidebar = document.querySelector('.sidebar'); 
-    const overlay = document.querySelector('.overlay'); 
-    sidebar.classList.remove('active'); 
-    overlay.classList.remove('active'); 
+overlay.addEventListener('click', function () {
+  sidebar.classList.remove('active');
+  overlay.classList.remove('active');
+});
+
+
+const editHotelBtn = document.getElementById('editHotelBtn');
+const hotelNameInput = document.getElementById('hotelName');
+
+editHotelBtn.addEventListener('click', function () {
+  if (hotelNameInput.hasAttribute('readonly')) {
+    hotelNameInput.removeAttribute('readonly'); 
+    editHotelBtn.textContent = 'Save'; 
+  } else {
+    hotelNameInput.setAttribute('readonly', 'readonly'); 
+    editHotelBtn.textContent = 'Edit'; 
+  }
+});
+
+
+const editAddressBtn = document.getElementById('editAddressBtn');
+const hotelAddressInput = document.getElementById('hotelAddress');
+
+editAddressBtn.addEventListener('click', function () {
+  if (hotelAddressInput.hasAttribute('readonly')) {
+    hotelAddressInput.removeAttribute('readonly'); 
+    editAddressBtn.textContent = 'Save'; 
+  } else {
+    hotelAddressInput.setAttribute('readonly', 'readonly'); 
+    editAddressBtn.textContent = 'Edit'; 
+  }
 });
